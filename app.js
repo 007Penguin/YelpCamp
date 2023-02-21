cb3f56691019873fc928051ec7a9cc3fccf1b08d
Penglong <rosa071088@gmail.com>
2023-02-21 18:04:49 +0000

    Common Security Issues (before helmet)

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
+// const helmet = require('helmet')
 
 
 const Campground = require('./models/campground');
@@ -42,19 +44,23 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded ({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join (__dirname, 'public')))
+app.use(mongoSanitize());
 
 const sessionConfig = {
+    name: 'session',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
 app.use(session(sessionConfig))
 app.use(flash());
+// app.use(helmet());
 
 app.use(passport.initialize());
 app.use(passport.session());
