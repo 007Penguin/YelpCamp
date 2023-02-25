6c8197349d34b31109a71a8926b990877a9a3319
Penglong <rosa071088@gmail.com>
2023-02-25 10:25:26 +0000

    deploying 2.2

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(mongoSanitize({
     replaceWith: '_'
 }))
 
-const secret = process.env.SECRET || 'thisshouldbeabettersecret!',
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!'
 
 const store = new MongoStore ({
     url: dbUrl,
